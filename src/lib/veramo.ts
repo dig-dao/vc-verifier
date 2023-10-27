6d767fc70cdc588a70fca5ed328fa706e4b83751
kitakaze-kan <kantaro.1013.fujimori@gmail.com>
2023-10-27 11:20:20 +0900

    fix:added credentialLD veramo plugin

--- src/lib/veramo.ts
+++ src/lib/veramo.ts
@@ -5,6 +5,14 @@ import { Resolver } from "did-resolver";
 import { getResolver as pkhDidResolver } from "pkh-did-resolver";
 import { getResolver as webDidResolver } from "web-did-resolver";
 import { CredentialIssuerEIP712 } from "@veramo/credential-eip712";
+import {
+  CredentialIssuerLD,
+  LdDefaultContexts,
+  VeramoEcdsaSecp256k1RecoverySignature2020,
+  VeramoEd25519Signature2018,
+  VeramoEd25519Signature2020,
+  VeramoJsonWebSignature2020,
+} from "@veramo/credential-ld";
 
 export const getAgent = () => {
   return createAgent<IResolver & ICredentialPlugin>({
@@ -17,6 +25,15 @@ export const getAgent = () => {
       }),
       new CredentialPlugin(),
       new CredentialIssuerEIP712(),
+      new CredentialIssuerLD({
+        contextMaps: [LdDefaultContexts],
+        suites: [
+          new VeramoEcdsaSecp256k1RecoverySignature2020(),
+          new VeramoJsonWebSignature2020(),
+          new VeramoEd25519Signature2020(),
+          new VeramoEd25519Signature2018(),
+        ],
+      }),
     ],
   });
 };
